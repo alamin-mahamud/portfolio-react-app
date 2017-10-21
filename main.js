abc5a9db977011b2f90a7201643ddbf93514c0d8
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2017-10-22 00:16:46 +0600

    complete design renewal

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-
-  var strings = [/*['W','e','b', ' ', 'A', 'p', 'p', 'l', 'i', 'c', 'a', 't', 'i', 'o', 'n', 's']]*/'Web Applications', 'Desktop Applications', 'Web APIs', 'Distributed Systems'];
-  var cout = 1;
-  $('#change').html(strings[0]);
-  setInterval(function() {
-    $('#change').html(strings[cout]);
-    cout == strings.length-1 ? cout = 0 : cout++;
-  }, 2000);
+    var strings = [ /*['W','e','b', ' ', 'A', 'p', 'p', 'l', 'i', 'c', 'a', 't', 'i', 'o', 'n', 's']]*/ 'Web Applications', 'Desktop Applications', 'Web APIs', 'Distributed Systems'];
+    var cout = 1;
+    var $change = $("h2#introChange");
+    console.log($change);
+    $change.html(strings[0]);
+    setInterval(function() {
+        $change.html(strings[cout]);
+        cout == strings.length - 1 ? cout = 0 : cout++;
+    }, 2000);
 
 });
