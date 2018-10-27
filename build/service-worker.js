d3c1303e6b3c2f7c9417486f9f440007ca150511
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-27 18:24:55 +0600

    :fire: Portfolio react app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34784b6a4f9ad486cfbcd33e57902109.js"
+  "/website/precache-manifest.00537eaa16843302b630744f50422ce7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
