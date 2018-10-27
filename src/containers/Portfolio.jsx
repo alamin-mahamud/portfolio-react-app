64ee672d0b37769d9e21b066df09274607dfb390
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-28 00:18:54 +0600

    Updates

--- src/containers/Portfolio.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Portfolio = () => <h2>Portfolio</h2>;
-
-export default Portfolio;
--- src/containers/Projects.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Projects = () => <h2>Projects</h2>
-
-export default Projects;
