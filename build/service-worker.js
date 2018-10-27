4ceab9c332a5dd764266d0cb573e0fc7bdd8ab26
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-27 18:29:56 +0600

    :art: update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.00537eaa16843302b630744f50422ce7.js"
+  "/portfolio-react-app/precache-manifest.f477900b80afd1f866654e81a13e9ef1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
