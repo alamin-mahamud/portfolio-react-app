61d826aae3248841be431aa479f384164e843818
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-28 21:03:36 +0600

    :construction: trying to deploy heroku

---
+++ build/precache-manifest.f477900b80afd1f866654e81a13e9ef1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82922a53ddcd8a6bfd0a",
+    "url": "/portfolio-react-app/static/js/runtime~main.82922a53.js"
+  },
+  {
+    "revision": "40314e26e3d9869c9750",
+    "url": "/portfolio-react-app/static/js/main.40314e26.chunk.js"
+  },
+  {
+    "revision": "312a3f4ad9b0b53018de",
+    "url": "/portfolio-react-app/static/js/1.312a3f4a.chunk.js"
+  },
+  {
+    "revision": "40314e26e3d9869c9750",
+    "url": "/portfolio-react-app/static/css/main.518c7cc2.chunk.css"
+  },
+  {
+    "revision": "58d962e87fc8c5192aca0fa3b51cc477",
+    "url": "/portfolio-react-app/index.html"
+  }
+];
